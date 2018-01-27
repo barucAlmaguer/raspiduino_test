2f1168df3b807b119bfa0485e482964c6fbbf971
alde103 <aldebaran_103@hotmail.com>
2018-01-27 13:58:16 -0600

    Archivo .ino

---
+++ firmware/raspiduino_test/raspiduino_test.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // start serial port at 9600 bps:
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+ }
+
+void loop() {
+  char x=Serial.read();
+  if (x=='n')
+  {
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  }
+  else if (x=='y')
+  {
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  }
+}
