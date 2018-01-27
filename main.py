702a6136c3af22658c315267a1680ba60cc42c59
baruc <baruc.almaguer@gmail.com>
2018-01-27 14:22:11 -0600

    portnames

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import serial
+import serial.tools.list_ports as lp
+
+def portnames():
+    lista = lp.comports()
+    names = []
+    for port in lista:
+        names.append(port[0])
+    return names
+
 print("hola mundo")
